9888fef2de5a732e4706a07824362653c0a81975
Alexey Leshchenko <leshchenko@gmail.com>
2024-04-18 21:59:17 +0300

    Allocate less vCPU

--- make.bat
+++ make.bat
@@ -1,4 +1,8 @@
 docker build --pull --rm -f "Dockerfile" -t loans:latest "."
 docker tag loans:latest cr.yandex/crp8ek2lo6uuvnveblac/loans
 docker push cr.yandex/crp8ek2lo6uuvnveblac/loans
-yc serverless container revision deploy --container-name loans --image cr.yandex/crp8ek2lo6uuvnveblac/loans --cores 1 --memory 256MB --concurrency 1 --execution-timeout 20s --service-account-id ajeeqpdslsj7pt0usup8
+
+yc serverless container revision deploy ^
+--container-name loans --image cr.yandex/crp8ek2lo6uuvnveblac/loans ^
+--cores 1 --core-fraction 5 --memory 256MB --concurrency 1 ^
+--execution-timeout 20s --service-account-id ajeeqpdslsj7pt0usup8
